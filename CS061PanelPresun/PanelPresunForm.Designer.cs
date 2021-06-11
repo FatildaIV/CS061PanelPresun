34205877158d1a14664b0f0fd309dbd2fa0eb9a1
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 14:32:14 +0200

    Initial commit

---
+++ CS061PanelPresun/PanelPresunForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CS061PanelPresun
+{
+    partial class PanelPresunForm
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel1.Location = new System.Drawing.Point(141, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(422, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // PanelPresunForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "PanelPresunForm";
+            this.Text = "CS061 Přesun Panel - Oldřich Baudyš";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PanelPresunForm_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CS061PanelPresun/PanelPresunForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS061PanelPresun
+{
+    public partial class PanelPresunForm : Form
+    {
+        public PanelPresunForm()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            //panel1.Parent.MouseMove
+            PanelPresunForm_MouseMove(
+                sender,
+                new MouseEventArgs(
+                    e.Button, e.Clicks, e.X + panel1.Left, e.Y + panel1.Top, e.Delta));
+            //if (e.Button == MouseButtons.Left)
+            //{
+            //    //panel1.Location = new Point(Cursor.Position.X + e.X, Cursor.Position.Y + e.Y);
+            //    panel1.Left = Cursor.Position.X + e.X;
+            //    panel1.Top = Cursor.Position.Y + e.Y;
+            //}
+        }
+
+        private void PanelPresunForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            //label1.Text = string.Format("x: {0}, y: {1} ", e.X, e.Y);
+            label1.Text = string.Format("{0}, {1}, {2}", e.Location, e.Delta, Cursor.Position);
+            if (e.Button == MouseButtons.Left)
+            {
+                panel1.Top = e.Y;
+                panel1.Left = e.X;
+            }
+        }
+
+    }
+}
